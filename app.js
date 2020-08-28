6b13a36570eb594e21b00205b4f47c74b8db3c83
abdulaziz <alzaabi98@gmail.com>
2020-08-28 21:05:43 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3003
+
+app.get('/', (req,res)=> {
+    res.send('started')
+})
+
+app.listen(port , ()=> {
+    console.log(`startred at port ${port}`)
+})
+
